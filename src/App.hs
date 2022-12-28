56df102b70cedf4277335ada16aaff3bcbf15270
yskoht <ysk.oht@gmail.com>
2022-12-29 01:34:36 +0900

    add showBoard

--- src/App.hs
+++ src/App.hs
@@ -1,6 +1,23 @@
 module App
-    ( app
-    ) where
+  ( app
+  ) where
+
+import Control.Monad
+
+import Types
+import SplitBy
+import Readable
+
+initBoard :: Board
+initBoard = replicate 16 Empty
+
+showBoard :: Board -> IO ()
+showBoard board = do
+  let _lines = splitBy4 $ readable board
+  forM_ _lines $ \line -> do
+    putStrLn $ show line
 
 app :: IO ()
-app = putStrLn "someFunc"
+app = do
+  showBoard initBoard
+  showBoard $ map (\x -> Number x) [1..16]
