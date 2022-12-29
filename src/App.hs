97ef6bc6fc08ff1fbf5f74a352412f60a8e3f7e9
yskoht <ysk.oht@gmail.com>
2022-12-29 18:28:29 +0900

    add play

--- src/App.hs
+++ src/App.hs
@@ -3,9 +3,21 @@ module App
   ) where
 
 import Control.Monad.State
+import System.IO
 
+import Types
 import Board
 
 app :: IO ()
 app = do
-  execStateT initBoard emptyBoard >>= showBoard
+  hSetBuffering stdin NoBuffering
+  board <- execStateT initBoard emptyBoard
+  play board
+
+play :: Board -> IO()
+play board = do
+  c <- getChar
+  showBoard board
+  case c of
+    'q' -> return ()
+    _ -> play board
