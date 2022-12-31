f2e239c874e32f30602cc2ceb34e3dcec58c96db
yskoht <ysk.oht@gmail.com>
2022-12-31 13:58:11 +0900

    add move'

--- src/Board.hs
+++ src/Board.hs
@@ -65,6 +65,46 @@ move key board
     moveAndSquash = squashLine . moveLine
     moveAndSquashRev = reverse . moveAndSquash . reverse
 
+type LabeledSquare = ([Int], Square)
+type LabeledBoard = [LabeledSquare]
+
+move' :: Key -> Board -> LabeledBoard
+move' key board
+  | key == LeftKey  = concatMap moveAndSquash' bs
+  | key == RightKey = concatMap moveAndSquashRev' bs
+  | key == UpKey    = concat $ transpose $ map moveAndSquash' $ transpose bs
+  | key == DownKey  = concat $ transpose $ map moveAndSquashRev' $ transpose bs
+  | otherwise = error ""
+  where
+    bs = splitBy4 $ labeled [0..] board
+    moveAndSquash' = squashLine' . moveLine'
+    moveAndSquashRev' = reverse . moveAndSquash' . reverse
+
+labeled :: [Int] -> [Square] -> [LabeledSquare]
+labeled = zipWith (\i s -> ([i], s))
+
+moveLine' :: [LabeledSquare] -> [LabeledSquare]
+moveLine' line =
+  numbers ++ labeled (repeat (-1)) (empties emptyNum)
+  where
+    emptyNum = length $ filter (isEmpty . snd) line
+    numbers = filter (not . isEmpty . snd) line
+
+squashLine' :: [LabeledSquare] -> [LabeledSquare]
+squashLine' xs =
+  let (as, bs, cs) = foldl squash ([], [], []) xs
+  in as ++ bs ++ cs
+  where
+    squash :: ([LabeledSquare], [LabeledSquare], [LabeledSquare]) -> LabeledSquare -> ([LabeledSquare], [LabeledSquare], [LabeledSquare])
+    squash (line, [], es) s = (line, [s], es)
+    squash (line, [s], es) t@(_, Empty) = (line ++ [s], [t], es)
+    squash (line, [u@(_, Empty)], es) t = (line ++ [u], [t], es)
+    squash (line, [u@(i, Number t)], es) v@(j, Number n) =
+      if t == n
+        then (line ++ [(i ++ j, Number (t + n))], [], es ++ [([-1], Empty)])
+        else (line ++ [u], [v], es)
+    squash _ _ = error ""
+
 moveLine :: [Square] -> [Square]
 moveLine line =
   numbers ++ empties emptyNum
@@ -109,3 +149,4 @@ isGameOver board = emptyNotExists board
   where
     _lines = splitBy4 board
     linesT = transpose _lines
+
