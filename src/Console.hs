f84d5872d99c1e91d05db9713cb29f18ffc5d5a7
yskoht <ysk.oht@gmail.com>
2023-01-02 14:44:30 +0900

    fix comment

--- src/Console.hs
+++ src/Console.hs
@@ -43,10 +43,10 @@ getKey = do
   case ord c of
     27 -> getKey
     91 -> getKey
-    65 -> return UpKey -- 27 29 65 -> Up
-    66 -> return DownKey -- 27 29 66 -> Down
-    67 -> return RightKey -- 27 29 67 -> Right
-    68 -> return LeftKey -- 27 29 68 -> Left
+    65 -> return UpKey -- 27 91 65 -> Up
+    66 -> return DownKey -- 27 91 66 -> Down
+    67 -> return RightKey -- 27 91 67 -> Right
+    68 -> return LeftKey -- 27 91 68 -> Left
     104 -> return LeftKey -- h
     106 -> return DownKey -- j
     107 -> return UpKey -- k
